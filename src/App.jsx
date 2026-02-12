e2890ea740db9238fec4c08530aff58e131f8acc
Denisa <denisafurtuna10@gmail.com>
2026-02-13 00:26:48 +0100

    add all project files

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from './section/Navbar'
+import Hero from './section/Hero'
+
+
+const App=()=>{
+  
+
+  return (
+    
+      <div className='container mx-auto max-w-7xl'>
+       <Navbar /> 
+     <Hero />
+     <section className="min-h-screen"/>
+     <section className="min-h-screen"/>
+     <section className="min-h-screen"/>
+     <section className="min-h-screen"/>
+     <section className="min-h-screen"/>
+     <section className="min-h-screen"/>
+     
+      {/*<About />*/}
+      { /*<Projects />*/ }
+      { /*<Experiences />*/ }
+      { /*<Testimonial />*/ }
+      { /*<Contact />*/ }
+      { /*<Footer/>*/ }
+          
+
+       
+      </div>
+     
+  )
+}
+
+export default App
---
+++ src/section/Hero.jsx
@@ -0,0 +1,35 @@
+import react from 'react'
+import HeroText from '../components/HeroText'
+import ParallaxBackground from '../components/ParallaxBackground'
+import { Canvas } from '@react-three/fiber'
+import { Astronaut } from '../components/Astronaut'
+import { OrbitControls } from '@react-three/drei'
+import { useMediaQuery } from 'react-responsive'
+
+const Hero = () => {
+    const isMobile = useMediaQuery({ maxWidth: 853 })
+    
+    return(
+        <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
+            <HeroText />
+            <ParallaxBackground/>
+            <figure className='absolute inset-0' style={{width:"100vw", height:"100vh"}}>
+                <Canvas camera={{ position: [0, 1, 3] }}>
+                    <ambientLight intensity={1} />
+                    <directionalLight position={[5, 5, 5]} intensity={1} />
+                    <pointLight position={[-5, 5, 5]} intensity={0.5} />
+                    
+                    <Astronaut 
+                        scale={isMobile ? 0.23 : 0.35} 
+                        position={isMobile ? [1.3, 1, 0] : [2, 1, 0]} 
+                        rotation={[0, 0, 0]}
+                    /> 
+                    <OrbitControls />
+                </Canvas>
+            </figure>
+        </section>
+    )
+}
+
+
+export default Hero
